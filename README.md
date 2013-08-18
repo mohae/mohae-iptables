ce2e737b3c93cc9e8da95d371f14945ff2cf1c79
Count Derp <thecount@moehae.com>
2013-08-18 14:42:20 -0500

    initial setup and commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# mohae-iptables cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: mohae-iptables
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'iptables'
